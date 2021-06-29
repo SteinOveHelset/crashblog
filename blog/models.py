43ed5e6d04abefd40b0b27842d00987445209563
Code With Stein <codewithstein@gmail.com>
2021-06-29 07:02:58 +0200

    part 4

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-created_at',)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .forms import CommentForm
+from .models import Post
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
